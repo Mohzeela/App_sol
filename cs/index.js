c51f13a34a38e72336ec11be21a9a6f13c65be8b
Wale Mohammed <66002774+Mohzeela@users.noreply.github.com>
2022-02-09 20:51:15 +0000

    Update index.js

--- cs/index.js
+++ cs/index.js
@@ -6,10 +6,10 @@ const { randomUUID } = require('crypto');
 //console.log(randomUUID());
 
 const connection = mysql.createConnection({
-    host: '20.107.41.216',
-    user: 'root',
+    host: '20.126.65.172',
+    user: 'adeleke',
     password: 'test1234',
-    database: 'mydb2'
+    database: 'mydb'
 });
 
 connection.connect((err)=>{
@@ -22,25 +22,29 @@ connection.connect((err)=>{
 //instatiate express library
 const app = express();
 
-let sql = `SELECT * FROM titanic_table`; 
+let sql = `SELECT * FROM mytable`; 
     connection.query(sql, (error, results, fields) => { 
         if (error) { 
             return console.error(error.message); 
         } 
-        console.log(results); 
-        if(results[0].uuid){
+         
+        const resaz = JSON.stringify(results);
+        const dd = results;
+        console.log(results);
+        if(results[1].uuid){
             console.log('uuid exist in table');
         }else{
-            connection.query("ALTER TABLE titanic_table ADD COLUMN uuid VarChar(50) NOT NULL",
+            connection.query("ALTER TABLE mytable ADD COLUMN uuid VarChar(50) NOT NULL",
             (error, results, fields) => { 
                 if (error) { 
                     return console.error(error.message); 
                 } 
-                console.log('updated the database with column uuid'); 
+                console.log('updated the database with column uuid');
+                console.log(results); 
             })
         }
     }); 
-    connection.end();
+    //connection.end();
 
     
 const person = { "survived": true, "passengerClass": 3, "name": "Mr. Owen Harris Braund", "sex": "male", "age": 22, "siblingsOrSpousesAboard": 1, "parentsOrChildrenAboard":0, "fare":7.25}
@@ -79,6 +83,6 @@ app.put('/people/{uuid}', (req, res)=> {
 })
 
 // start the api server and transmitting on port 4000
-app.listen(4000, ()=> {
+app.listen(4001, ()=> {
     console.log('Server is running on port 4000...');
 });
